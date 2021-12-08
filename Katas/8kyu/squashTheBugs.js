14e1c3346592157e71824602929c232583c73492
brunash <bobazso@yahoo.com>
2021-12-08 13:09:57 -0500

    done

---
+++ Katas/8kyu/squashTheBugs.js
@@ -0,0 +1,20 @@
+function findLongest(str) {
+    return Math.max(...str.split(' ').map(word=>word.length))
+}
+
+
+// ----------------------------
+
+
+function findLongest(str) {
+  var spl = str.split(" "),
+      longest = 0;
+  
+  for (var i in spl) {
+    if (spl[i].length > longest) {
+      longest = spl[i].length;
+    }
+  }
+
+  return longest;
+}
