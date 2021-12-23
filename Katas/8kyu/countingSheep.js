831dd851df296c76effdd66a5ac5e1ebe3884ad6
brunash <bobazso@yahoo.com>
2021-12-23 13:17:16 -0500

    done

---
+++ Katas/8kyu/countingSheep.js
@@ -0,0 +1,9 @@
+function countSheeps(arrayOfSheeps) 
+{
+    var num=0;
+    for(var i=0; i<arrayOfSheeps.length; i++)
+    {  
+      if(arrayOfSheeps[i]==true){ num++; }
+    }
+    return num;
+}
