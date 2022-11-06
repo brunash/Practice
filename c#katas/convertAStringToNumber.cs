3f9d05a9dbbfd982530e8dbf13cc795906ae3093
brunash <bobazso@yahoo.com>
2022-11-06 15:30:22 -0500

    kata

---
+++ c#katas/convertAStringToNumber.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Kata
+{
+    public static int StringToNumber(String str) {
+        int result;
+        result = Convert.ToInt32(str);
+        return result;
+    }
+}
