e4db881d28881e89b214c4a27ba39722404c73e4
brunash <bobazso@yahoo.com>
2022-11-23 10:00:49 -0500

    kata

---
+++ c#katas/grasshopperMessiGoalsFunction.cs
@@ -0,0 +1,8 @@
+public class Kata
+{
+    public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+    {
+        int total;
+        return total = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
